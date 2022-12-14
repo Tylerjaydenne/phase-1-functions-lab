6cb6ff1f3d2f25156ff436a32c882deb62be5e08
Tylerjaydenne <tylerjaydenne127@gmail.com>
2022-12-14 11:18:29 -0500

    commit tree 2138307a72def4b802b2da05a0538ecd306ee2cc

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+let ft = 264
+let HQ = 42
+
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue-HQ)
+}
+
+function distanceFromHqInFeet(blocks){
+    let difference = distanceFromHqInBlocks(blocks);
+
+    return difference * ft 
+
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let difference = Math.abs(start - destination)
+    return difference * ft
+}
+function calculatesFarePrice(start, destination) {
+    
+    let difference = distanceTravelledInFeet(start, destination)
+
+    if(difference > 2500) {
+        return 'Cannot travel that far.'
+    } else if (difference < 400) {
+        return 'This is free.'
+    }
+        return difference * 2  
+    }
+        
+
+        
